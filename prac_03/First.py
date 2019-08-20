1335527173c93d045bcdfc616990c8ed8b4b9b22
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-08-20 10:44:15 +1000

    all previous prac files before week 4

--- prac_03/First.py
+++ prac_03/First.py
@@ -1,10 +1,20 @@
-password = input("enter a password:")
+def main():
 
-while len(password) < 10:
-    print("password is not long enough")
-    password = input("re-enter a password:")
+    password = input("enter a password:")
 
-for i in range(len(password)):
-    print('*', end='')
+    password = get_password(password)
 
+    get_length(password)
+
+
+def get_length(password):
+    for i in range(len(password)):
+        print('*', end='')
+
+
+def get_password(password):
+    while len(password) < 10:
+        print("password is not long enough")
+        password = input("re-enter a password:")
+    return password
 
