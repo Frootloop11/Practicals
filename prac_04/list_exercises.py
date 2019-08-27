95aeeb743562a6def954e2c3554b22743074a159
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-08-27 10:46:02 +1000

    username

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,13 +1,25 @@
-numbers = []
+# numbers = []
+#
+# for i in range(5):
+#     add_number = int(input("add a number"))
+#     numbers.append(add_number)
+#
+# print(numbers)
+#
+# print("the first number is: {}".format(numbers[0]))
+# print("the last number is: {}".format(numbers[-1]))
+# print("the smallest number is: {}".format(min(numbers)))
+# print("the largest number is: {}".format(max(numbers)))
+# print("the average number is: {}".format(sum(numbers) / len(numbers)))
 
-for i in range(5):
-    add_number = int(input("add a number"))
-    numbers.append(add_number)
+print("case sensitive")
 
-print(numbers)
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-print("the first number is: {}".format(numbers[0]))
-print("the last number is: {}".format(numbers[-1]))
-print("the smallest number is: {}".format(min(numbers)))
-print("the largest number is: {}".format(max(numbers)))
-print("the average number is: {}".format(sum(numbers) / len(numbers)))
+enter_username = input("enter user name:")
+
+while enter_username not in usernames:
+    print("access denied")
+    enter_username = input("enter user name:")
+print("Access granted")
