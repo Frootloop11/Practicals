2620e9629d9c7b3173a86f318ffa73c574af510b
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-08-27 10:35:19 +1000

    second exercise

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,4 +6,8 @@ for i in range(5):
 
 print(numbers)
 
-
+print("the first number is: {}".format(numbers[0]))
+print("the last number is: {}".format(numbers[-1]))
+print("the smallest number is: {}".format(min(numbers)))
+print("the largest number is: {}".format(max(numbers)))
+print("the average number is: {}".format(sum(numbers) / len(numbers)))
