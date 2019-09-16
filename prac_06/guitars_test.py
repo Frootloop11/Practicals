1709a37eee5c85671a4c9b50b99d802697c21793
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-09-17 09:29:15 +1000

    complete prac_06

--- prac_06/guitars_test.py
+++ prac_06/guitars_test.py
@@ -6,14 +6,14 @@ CURRENT_YEAR = 2019
 
 def run_tests():
     name = "Gibson L-5 CES"
-    year = 1922
+    year = 1924
     cost = 16035.40
 
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2012, 1512.9)
 
-    print("{} get_age() - Expected {}. got {}". format(guitar.name, 95, Guitar.get_age))
-    print("{} get_age() - Expected {}. got {}". format(other.name, 5, other.get_age()))
+    print("{} get_age() - Expected {}. got {}". format(guitar.name, 95, guitar.get_age()))
+    print("{} get_age() - Expected {}. got {}". format(other.name, 7, other.get_age()))
     print()
     print("{} is_vintage() - Expected {}. Got {}".format(guitar.name, True, guitar.is_vintage()))
     print("{} is_vintage() - Expected {}. Got {}".format(other.name, False, other.is_vintage()))
